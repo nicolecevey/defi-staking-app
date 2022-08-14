44571afeca78747c15b4a646d854083ef7e4b97b
Nicole Cevey <nicole.cevey@gmail.com>
2022-08-13 23:23:42 -0400

    Creating smart contract

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -1,20 +1,21 @@
 pragma solidity ^0.5.0;
 
 contract Tether {
-    string public name = "Tether";
-    string public symbol = "USDT";
+    string public name = "Mock Tether Token";
+    string public symbol = "mUSDT";
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-    event Approve(
+    event Approval(
         address indexed _owner,
         address indexed _spender,
         uint256 _value
     );
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor() {
         balanceOf[msg.sender] = totalSupply;
@@ -30,6 +31,31 @@ contract Tether {
         balanceOf[msg.sender] -= _value;
         // add the balance
         balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        // Add the balance for transferFrom
+        balanceOf[_to] += _value;
+        // Substract the balance for transferFrom
+        balanceOf[_from] += _value;
+        allowance[msg.sender][_from] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
     }
