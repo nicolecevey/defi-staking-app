823e3d9d75043917ab9c586fb27763beddda3419
Nicole Cevey <nicole.cevey@gmail.com>
2022-08-14 23:18:58 -0400

    Adding testing for decentral bank

---
+++ test/decentralBank.tests.js
@@ -0,0 +1,60 @@
+const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .should();
+
+contract("DecentralBank", ([owner, customer]) => {
+  // Initialize variables
+  let tether, rwd, decentralBank;
+
+  function tokens(number) {
+    return web3.utils.toWei(number, "ether");
+  }
+
+  // Runs before anything else
+  before(async () => {
+    // Load contracts
+    tether = await Tether.new();
+    rwd = await RWD.new();
+    decentralBank = await DecentralBank.new(rwd.address, tether.address);
+
+    // Transfer all tokens to DecentralBank (1 million)
+    await rwd.transfer(decentralBank.address, tokens("1000000"));
+
+    // Transfer 100 mock Tethers to customer
+    await tether.transfer(customer, tokens("100"), { from: owner });
+  });
+
+  // Tether testing
+  describe("Mock Tether Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await tether.name();
+      assert.equal(name, "Mock Tether Token");
+    });
+  });
+  // RWD testing
+  describe("Reward Token Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await rwd.name();
+      assert.equal(name, "Reward Token");
+    });
+    it("matches symbol successfully", async () => {
+      const symbol = await rwd.symbol();
+      assert.equal(symbol, "RWD");
+    });
+  });
+  // Decentral Bank testing
+  describe("Decentral Bank Deployment", async () => {
+    it("matches name successfully", async () => {
+      const name = await decentralBank.name();
+      assert.equal(name, "Decentral Bank");
+    });
+    it("contract has tokens", async () => {
+      let balance = await rwd.balanceOf(decentralBank.address);
+      assert.equal(balance, tokens("1000000"));
+    });
+  });
+});
